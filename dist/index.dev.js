1d8d5ad2365d3bf28b5fad7498e43d18eb0333aa
cjdomacena@yahoo.com <cjdomacena@yahoo.com>
2020-12-15 05:02:45 -0500

    Added scrollspy, second project, resume

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,1 +1,19 @@
 "use strict";
+
+var faders = document.querySelectorAll('.fade-in');
+var appearOptions = {
+  threshold: 0.5
+};
+var appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+faders.forEach(function (fader) {
+  appearOnScroll.observe(fader);
+});
